53997c56308cf5c712cd9f64cdb9c6556ea7c876
JonOuellette <jonathan.t.ouellette@gmail.com>
2023-12-04 17:43:32 -0800

    lunchly assignment - created full name method on the customer class.  Created save method on reservation class.

---
+++ db.js
@@ -0,0 +1,14 @@
+/** Database for lunchly */
+
+const {Client} = require("pg");
+
+// const db = new pg.Client("postgresql:///lunchly");
+
+let db = new Client({
+    host: "/var/run/postgresql",
+    database: "lunchly"
+  });
+
+db.connect();
+
+module.exports = db;
